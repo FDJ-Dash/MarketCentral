5ccdd8f59d8f1edb45f79553a2f8a6447dc1f940
Fernando Daniel Jaime <fernando.daniel.jaime@gmail.com>
2018-10-25 23:09:33 -0300

    MarketCentral initial

---
+++ app/src/main/java/com/freetime_software/marketcentral/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.freetime_software.marketcentral.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.freetime_software.marketcentral.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
