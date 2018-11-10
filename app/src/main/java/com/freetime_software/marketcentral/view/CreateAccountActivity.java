dea12ca86c64eb2f93ec0e75d1a8df56c541d1da
Fer <fernando.daniel.jaime@gmail.com>
2018-11-10 20:13:16 -0300

    Login and create account

--- app/src/main/java/com/freetime_software/marketcentral/view/CreateAccountActivity.java
+++ app/src/main/java/com/freetime_software/marketcentral/view/CreateAccountActivity.java
@@ -1,7 +1,9 @@
 package com.freetime_software.marketcentral.view;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.freetime_software.marketcentral.R;
 
@@ -11,5 +13,16 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_createaccount),true);
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void showToolbar(String tittle, boolean upButton) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(tittle);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+        }
     }
 }
